ebcd075b1cc2e7e49a2f5ee087a025bf4e9d143f
JulieLeslie <julie@businesslatitude.com.au>
2015-05-21 14:52:27 +1000

    Added comments and change the coding a little on reflection

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+# I am really not sure about this assignment as the logic behind these
+# two functions and the example of assignment seems incredibly contrived
+# especially when there are much easier ways to do this
+# Never the less, here is my offering
 
-## Write a short comment describing this function
+#The first function creates a list of functions so you can set a matrix,
+# get that matrix again and also calculate the inverse of that matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
+	#set the value of m to null so that it is definitely contains the value you want
+	m<-NULL
+	
+	#set your matrix into the variable
+	set<-function(y){
+		#through lexical scoping this is available in the function above, but not the environment
+		#that is, I can use it in the other functions but I wont see it in the environment
 		x<<-y
 		m<<-NULL
 	}
-get<-function() x
-setmatrix<-function(z) m<<- solve(z)
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
+	
+	# returns the value of the inputed matrix
+	get<-function() x
+	
+	# create the inverse of the matrix, but check it is there first (lexical scoping!)
+	setmatrix<-function() {
+		if (is.null(x)){
+			message("you haven't set the matrix yet")
+		}
+		else {
+			m<<- solve(x)
+		}
+	}	
+	
+	# returns the value of the inverse matrix
+	getmatrix<-function() m
+	
+	#return a list of all the functions and matricies
+	list(set=set, get=get,
+	setmatrix=setmatrix,
+	getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverse matrix has been created
+# input is the list that was created in makeCacheMatrix
 
-cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
+cacheSolve <- function(x=list(), ...) {
+    # get the value of the inverse matrix
+	m <- x$getmatrix()
+	n <- x$get()
+	#if it is not null then return the value of the inverse matrix
+	# don't calculate it again
     if(!is.null(m)){
-		message("getting cached data")
 		return(m)
-    }
-    matrix<-x$get()
-    m<-solve(matrix, ...)
-    x$setmatrix(m)
-    m
+    }	
+	#Otherwise get the value of matrix and calculate
+	else if (!is.null(n)) {
+		n <- solve(n)
+		return(n)
+	}
+	#If that doesn't work then let the user know
+	else {
+		message("It has all gone badly wrong")
+	}
 }
 
